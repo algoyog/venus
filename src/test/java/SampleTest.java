9027eca3f9a223bbe9a2bc864af165c5828f27e8
Aravind B <aravindb@Aravinds-MacBook-Pro.local>
2022-04-30 10:23:44 +0530

    Added Junit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+    @Test
+    void sampleTest(){
+        assertEquals(2,2);
+    }
+}
